df5f2376f4511f42770faf0dd094f54a0499dc0a
Brittany Barnes <brittany@Zenny-1.lan1>
2020-01-28 16:30:13 -0500

    debug

--- public/index.php
+++ public/index.php
@@ -48,12 +48,21 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 | and wonderful application we have prepared for them.
 |
 */
+try {
+    $app->run();
+} catch(\Exception $e) {
+    echo "<pre>";
+    echo $e;
+    echo "</pre>";
+}
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
 );
+
+
 
 $response->send();
 
